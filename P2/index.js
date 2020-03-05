0bae644ec9e5395e1bd335bd8f0f1971f97f661e
Ignacio Cruz De La Haza <nachocru@f-l3210-pc24.aulas.etsit.urjc.es>
2020-03-05 09:40:09 +0100

    Avanzando en la calculadora

--- P2/index.js
+++ P2/index.js
@@ -1,12 +1,69 @@
 console.log("Ejecutando JS");
 
-const test = document.getElementById('test1')
-const test2 = document.getElementById('test2')
-mi_variable = 0;
-
-test1.onclick = () => {
-  console.log("1");
+//-- Estados de la calculadora
+const ESTADO = {
+  INIT: 0,
+  OP1: 1,
+  OPERATION: 2,
+  OP2_INIT: 3,
+  OP2: 4,
 }
-test2.onclick = () => {
-  console.log("2");
+let estado = ESTADO.INIT;
+let digitos = document.getElementsByClassName("cdigito");
+let operaciones = document.getElementsByClassName("operacion");
+for (i = 0; i<digitos.length; i++) {
+
+    digitos[i].onclick = (ev) =>{
+      if (estado == ESTADO.INIT) {
+        console.log("entro");
+        estado = ESTADO.OP1;
+        } else if (estado == ESTADO.OPERATION) {
+          console.log("Op 2");
+        estado = ESTADO.OP2;
+        }
+    digito(ev.target);
+    }
+}
+
+for (i = 0; i<operaciones.length; i++) {
+  operaciones[i].onclick = (ev) =>{
+    if (estado == ESTADO.INIT) {
+      console.log("incorrecto");
+      estado = ESTADO.INIT;
+    } else if (estado == ESTADO.OP1 || estado == ESTADO.OP2) {
+      console.log("OPERACION");
+      digito(ev.target);
+      estado = ESTADO.OPERATION;
+      }
+  }
+}
+function digito(boton)
+{
+  if (display.innerHTML=="0") {
+    display.innerHTML = boton.value;
+  } else {
+    display.innerHTML += boton.value;
+  }
+
+}
+
+
+//-- Evaluar la expresion
+igual.onclick = () => {
+  if (estado == ESTADO.INIT) {
+    console.log("No puedo evaluar");
+    estado = ESTADO.INIT;
+  } else if (estado == ESTADO.OP1 || estado == ESTADO.OP2) {
+    console.log("OPERACION realizada");
+    display.innerHTML = eval(display.innerHTML);
+    estado = ESTADO.INIT;
+  } else if (estado == ESTADO.OPERATION) {
+    console.log("No puedo evaluar")
+  }
+
+}
+
+//-- Poner a cero la expresion
+clear.onclick = () => {
+  display.innerHTML = "0";
 }

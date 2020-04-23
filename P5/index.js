43902b62dcc5e378d58161c3dd38d4ab11b1512b
nachocru <i.cruzd.2017@alumnos.urjc.es>
2020-04-23 10:51:48 +0200

    Avanzando P5

--- P5/index.js
+++ P5/index.js
@@ -4,12 +4,15 @@ console.log("Ejecutando JS....")
 const canvas = document.getElementById('canvas');
 const img = document.getElementById('imagesrc');
 const ctx = canvas.getContext('2d');
+const Grises = document.getElementById("Grises");
+const Colores = document.getElementById("Colores");
+var estado = "colores";
 
 //-- Acceso al deslizador
-const deslizador = document.getElementById('deslizador');
+const deslizador_R = document.getElementById('deslizador_R');
 
 //-- Valor del deslizador
-const range_value = document.getElementById('range_value');
+const range_value_R = document.getElementById('range_value_R');
 
 //-- Función de retrollamada de imagen cargada
 //-- La imagen no se carga instantaneamente, sino que
@@ -29,11 +32,46 @@ img.onload = function () {
   console.log("Imagen lista...");
 };
 
+Grises.onclick = () => {
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
+  estado = "grises";
+  ctx.drawImage(img, 0,0);
+
+  //-- Obtener la imagen del canvas en pixeles
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  //-- Obtener el array con todos los píxeles
+  let data = imgData.data
+
+  //-- Filtrar la imagen según el nuevo umbral
+  for (let i = 0; i < data.length; i+=4) {
+    brillo = (3 * data[i] + 4 * (data[i+1]) + (data[i+2]))/8
+    data[i] = brillo;
+    data[i+1] = brillo;
+    data[i+2] = brillo;
+  }
+  //-- Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0, 0);
+
+}
+
+Colores.onclick = () => {
+  estado = "colores";
+  ctx.putImageData(transformar_R(), 0, 0);
+}
 
 //-- Funcion de retrollamada del deslizador
-deslizador.oninput = () => {
+deslizador_R.oninput = () => {
+  if (estado == "colores"){
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(transformar_R(), 0, 0);
+
+  }
+}
+function transformar_R() {
   //-- Mostrar el nuevo valor del deslizador
-  range_value.innerHTML = deslizador.value;
+  range_value_R.innerHTML = deslizador_R.value;
 
   //-- Situar la imagen original en el canvas
   //-- No se han hecho manipulaciones todavia
@@ -46,7 +84,7 @@ deslizador.oninput = () => {
   let data = imgData.data
 
   //-- Obtener el umbral de rojo del desliador
-  umbral = deslizador.value
+  umbral = deslizador_R.value
 
   //-- Filtrar la imagen según el nuevo umbral
   for (let i = 0; i < data.length; i+=4) {
@@ -55,7 +93,7 @@ deslizador.oninput = () => {
   }
 
   //-- Poner la imagen modificada en el canvas
-  ctx.putImageData(imgData, 0, 0);
-}
+  return imgData;
 
+}
 console.log("Fin...");

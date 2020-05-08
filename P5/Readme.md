2b39de7760423c93c3cf91006ff7bfe91db3960f
nachocru <i.cruzd.2017@alumnos.urjc.es>
2020-05-08 12:41:11 +0200

    P5 acabada

--- P5/Readme.md
+++ P5/Readme.md
@@ -1,1 +1,27 @@
 # Práctica 5
+En esta práctica hemos implementado diferentes funciones
+y filtros para modificar una imagen.
+En primer lugar disponemos de 2 imágenes originales y podremos seleccionar
+cuál de ellas modificar pulsando el botón correspondiente de cada foto.
+Es decir, si pulsamos el botón "Seleccionar imagen 1", esta imagen se
+mostrará en el canvas de imagen manipulada y será la que modificaremos
+pulsando en las diferentes opciones. Lo mismo ocurre con la imagen 2.
+En cualquier momento podemos cambiar de imagen y esta se mostrará
+en su forma original.
+A continuación disponemos de varias opciones para modificar las imágenes:
+- Grises: Toma la imagen que estuviera en el canvas (ya sea la original
+o con alguna otra modificación) y la pone en escala de grises.
+- Colores: Muestra la imagen original y permite modificar cada uno de los
+valores de sus componentes RGB en función de los umbrales que presenten los
+deslizadores. Es decir, si el deslizador de la componente R está en 100,
+ningún píxel podrá superar ese umbral 100 en su componente R.
+Los deslizadores solo funcionan cuando esta opción está seleccionada.
+- Especular: Muestra la imagen en forma de espejo.
+- Invertir: Invierte los píxeles de la imagen de manera que la esquina
+superior izquierda pasa a ser la esquina inferior derecha.
+- Negativo: Muestra la imagen original con sus componentes de color RGB
+en negativo. Esto es, el negativo de cada componente es su valor complementario
+hasta llegar a 255 (rango de colores para 8 bits). Así, un valor R puro a 255,
+pasará a ser un negro de valor 0.
+- Vintage: Se le aplica un filtro Sepia a la imagen original mediante una
+matriz de transformación,que produce el efecto visual de una imagen vintage.
--- P5/index.js
+++ P5/index.js
@@ -2,11 +2,16 @@ console.log("Ejecutando JS....")
 
 //-- Obtener elementos del DOM
 const canvas = document.getElementById('canvas');
-const img = document.getElementById('imagesrc');
+var img = document.getElementById('imagesrc');
 const ctx = canvas.getContext('2d');
 const Grises = document.getElementById("Grises");
 const Colores = document.getElementById("Colores");
 const Especular = document.getElementById("Especular");
+const Invertir = document.getElementById("Invertir");
+const Negativo = document.getElementById("Negativo");
+const Vintage = document.getElementById("Vintage");
+const Sel1 = document.getElementById("Sel1");
+const Sel2 = document.getElementById("Sel2");
 var estado = "colores";
 
 //-- Acceso al deslizador
@@ -18,6 +23,10 @@ const deslizador_B = document.getElementById('deslizador_B');
 const range_value_R = document.getElementById('range_value_R');
 const range_value_G = document.getElementById('range_value_G');
 const range_value_B = document.getElementById('range_value_B');
+
+var imagen = ctx.getImageData(0, 0, canvas.width, canvas.height);
+var actual_img = document.createElement("img");
+actual_img.data = imagen;
 
 //-- Función de retrollamada de imagen cargada
 //-- La imagen no se carga instantaneamente, sino que
@@ -35,19 +44,31 @@ img.onload = function () {
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
   console.log("Imagen lista...");
+
 };
 
+// Selecciono la foto que voy a modificar
+Sel1.onclick = () => {
+  img = document.getElementById('imagesrc');
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+}
+
+Sel2.onclick = () => {
+  img = document.getElementById('image2src');
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+}
+
 Grises.onclick = () => {
-  //-- Situar la imagen original en el canvas
-  //-- No se han hecho manipulaciones todavia
+
+  //-- Situar la imagen actual en el canvas
   estado = "grises";
-  ctx.drawImage(img, 0,0, canvas.width, canvas.height);
+  ctx.drawImage(actual_img, 0,0, canvas.width, canvas.height);
 
   //-- Obtener la imagen del canvas en pixeles
-  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
   //-- Obtener el array con todos los píxeles
-  let data = imgData.data
+  var data = imgData.data
 
   //-- Filtrar la imagen según el nuevo umbral
   for (let i = 0; i < data.length; i+=4) {
@@ -66,19 +87,19 @@ Colores.onclick = () => {
   ctx.putImageData(transformar(), 0, 0);
 
 }
+
 //-- Funcion de retrollamada del deslizador
 deslizador_R.oninput = () => {
   if (estado == "colores"){
     //-- Poner la imagen modificada en el canvas
-    ctx.putImageData(transformar(), 0, 0);
+    transformar();
 
   }
 }
-
 deslizador_G.oninput = () => {
   if (estado == "colores"){
     //-- Poner la imagen modificada en el canvas
-    ctx.putImageData(transformar(), 0, 0);
+    transformar();
 
   }
 }
@@ -86,15 +107,30 @@ deslizador_G.oninput = () => {
 deslizador_B.oninput = () => {
   if (estado == "colores"){
     //-- Poner la imagen modificada en el canvas
-    ctx.putImageData(transformar(), 0, 0);
+    transformar();
 
   }
 }
 
+// Distintos filtros a aplicar en la imagagen
+Invertir.onclick = () => {
+  estado = "invertir";
+  ctx.putImageData(filtrar(), 0, 0);
+}
+
 Especular.onclick = () => {
+  estado = "especular";
+  ctx.putImageData(filtrar(), 0, 0);
+}
 
-  ctx.putImageData(especular(), 0, 0);
+Negativo.onclick = () => {
+  estado = "negativo";
+  ctx.putImageData(filtrar(), 0, 0);
+}
 
+Vintage.onclick = () => {
+  estado = "vintage";
+  ctx.putImageData(filtrar(), 0, 0);
 }
 
 function transformar() {
@@ -108,16 +144,17 @@ function transformar() {
   ctx.drawImage(img, 0,0, canvas.width, canvas.height);
 
   //-- Obtener la imagen del canvas en pixeles
-  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
   //-- Obtener el array con todos los píxeles
-  let data = imgData.data
+  var data = imgData.data
+
 
   //-- Obtener el umbral de rojo del desliador
   umbral_R = deslizador_R.value
   umbral_G = deslizador_G.value
   umbral_B = deslizador_B.value
-  console.log(data);
   //-- Filtrar la imagen según el nuevo umbral
   for (let i = 0; i < data.length; i+=4) {
     if (data[i] > umbral_R)
@@ -128,46 +165,100 @@ function transformar() {
       data[i+2] = umbral_B;
   }
   //-- Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0, 0);
   return imgData;
 }
 
-function especular(){
+function filtrar() {
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
   ctx.drawImage(img, 0,0, canvas.width, canvas.height);
 
   //-- Obtener la imagen del canvas en pixeles
-  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
   //-- Obtener el array con todos los píxeles
-  let data = imgData.data;
-  let new_data = new Array(data.length);
-  for (let i = 0; i < data.length/2; i+=4) {
-    let temporal1 = data[i];
-    let contrario1 = data.length - i;
+  var data = imgData.data
 
-    let temporal2 = data[i+1];
-    let contrario2 = data.length - i + 1;
+  //-- Aplicamos el tipo de filtro elegido
+  if (estado == "invertir"){
+    for (let i = 0; i < data.length/2; i+=4) {
+      let temporal1 = data[i];
+      let contrario1 = data.length - i;
 
-    let temporal3 = data[i+2];
-    let contrario3 = data.length - i + 2;
+      let temporal2 = data[i+1];
+      let contrario2 = data.length - i + 1;
 
-    let temporal4 = data[i+3];
-    let contrario4 = data.length - i + 3;
+      let temporal3 = data[i+2];
+      let contrario3 = data.length - i + 2;
 
-    data[i] = data[contrario1];
-    data[contrario1] = temporal1;
+      let temporal4 = data[i+3];
+      let contrario4 = data.length - i + 3;
 
-    data[i+1] = data[contrario2];
-    data[contrario2] = temporal2;
+      data[i] = data[contrario1];
+      data[contrario1] = temporal1;
 
-    data[i+2] = data[contrario3];
-    data[contrario3] = temporal3;
+      data[i+1] = data[contrario2];
+      data[contrario2] = temporal2;
 
-    data[i+3] = data[contrario4];
-    data[contrario4] = temporal4;
+      data[i+2] = data[contrario3];
+      data[contrario3] = temporal3;
 
+      data[i+3] = data[contrario4];
+      data[contrario4] = temporal4;
+
+    }
+  } else if (estado == "especular"){
+    for (let i = 0; i < data.length; i+=1200) {
+      for (let j = 0; j < 600; j+=4) {
+        let temporal1 = data[i+j];
+        let contrario1 = i+1200-j;
+
+        let temporal2 = data[i+j+1];
+        let contrario2 = i+1200-j+1;
+
+        let temporal3 = data[i+j+2];
+        let contrario3 = i+1200-j+2;
+
+        let temporal4 = data[i+j+3];
+        let contrario4 = i+1200-j+3;
+
+        data[i+j] = data[contrario1];
+        data[contrario1] = temporal1;
+
+        data[i+j+1] = data[contrario2];
+        data[contrario2] = temporal2;
+
+        data[i+j+2] = data[contrario3];
+        data[contrario3] = temporal3;
+
+        data[i+j+3] = data[contrario4];
+        data[contrario4] = temporal4;
+      }
+
+    }
+  } else if (estado == "negativo"){
+    for (let i = 0; i < data.length; i+=4) {
+      data[i] = 255 - data[i];
+      data[i+1] = 255 - data[i+1];
+      data[i+2] = 255 - data[i+2];
+    }
+  } else if(estado == "vintage"){
+    for (let i = 0; i < data.length; i+=4) {
+      var r = data[i];
+      var g = data[i+1];
+      var b = data[i+2];
+
+      data[i] = (r*.393) + (g*.769) + (b*.189);
+      data[i+1] = (r*.349) + (g*.686) + (b*.168);
+      data[i+2] = (r*.272) + (g*.543) + (b*.131);
+    }
   }
-  //-- Poner la imagen modificada en el canvas
+  //-- Guardar la imagen 
+  imagen = imgData;
   return imgData;
-
 }
+
 console.log("Fin...");

d112377c5029f854e5a427f1214410252418ac54
nachocru <i.cruzd.2017@alumnos.urjc.es>
2020-05-17 13:11:27 +0200

    Finalizando P4

--- P4/Readme.md
+++ P4/Readme.md
@@ -1,1 +1,37 @@
 # Práctica 4
+Para esta práctica hemos implementado un realizador de TV.
+Cuando cargamos la práctica nos encontramos con un cuadrado
+gande que será en el que emitamos el vídeo elegido. Por defecto aparece
+una imagen estática, que se muestra cada vez que no hay un vídeo para reproducirse.
+Debajo del cuadrado grande tenemos cada una de las fuentes de vídeo
+que podremos ir seleccionando en función de las distintas opciones.
+Inicialmente todas las previsualizaciones de los vídeos están paradas, y no se
+muestra ningún vídeo en el cuadrado principal. Podemos hacer una de las siguientes
+funciones apretando en su correspondiente botón:
+- Iniciar modo normal: Esta es la opción principal del realizador. Cuando iniciamos
+este modo, se activarán las previsualizaciones de las fuentes de vídeo y se habilitará
+el poder seleccionar alguno de ellos para mostrarlo por la pantalla de vídeo en emisión.
+Hasta que no seleccionamos algún vídeo no se muestra ninguno, aunque sí podemos ver las
+previsualizaciones. El tiempo del vídeo principal coincide con el seleccionado.
+- Iniciar modo automático: En este modo, a diferencia del anterior, no podremos
+seleccionar el vídeo que queremos visualizar, sino que estos se irán alternando de
+manera consecutiva ( vídeo 1, 2 y 3) cada 10 segundos. Si pulsamos en seleccionar
+de cualquier vídeo, se ignorará y seguirá con el cambio automático. El tiempo del
+vídeo seleccionado también coincide con el de su previsualización.
+- Iniciar modo bucle: Este último modo lo que nos permite es repetir una secuencia de
+cualquiera de los vídeos con duración de 10 segundos. Por defecto si pulsamos esta
+opción al iniciar el programa se seleccionará el vídeo 1, pero podemos cambiar de
+vídeo en cualquier momento seleccionando en su botón correspondiente. Eso sí, el cambio
+de vídeo se evalúa cada 10 segundos, es decir, hasta que no transcurra ese tiempo no se
+cambiará de vídeo si hemos seleccionado otro. Ejemplo: Selecciono el modo bucle y por
+defecto se repetirá la secuencia del vídeo 1 del segundo 1 al 10. Justo en el segundo 3
+pulso en el vídeo 2, por lo que transcurridos los 10 primeros segundos, se reproducirá
+en bucle el vídeo 2 del segundo 3 al 13. En este modo el tiempo del vídeo en emisión
+no coincide con los de las previsualizaciones, sino que se tiene en cuenta el momento en
+el que se pulsó el click del vídeo.
+- Emisión en pruebas: Por último hemos implementado un cuarto canal de vídeo que se puede
+seleccionar en cualquier momento ( tanto al inicio como después de haber seleccionado cualquier
+modo) que muestra una imagen estática de emisión en pruebas. Este modo nos devuelve por defecto
+al modo normal, por lo que inmediatamente después de pulsar este canal, se puede seleccionar
+cualquier otro como si estuviéramos en el modo normal. Si queremos volver al modo automático o
+al modo bucle deberemos seleccionar en iniciar modo automático/bucle.
--- P4/index.js
+++ P4/index.js
@@ -13,31 +13,52 @@ video2.height=100;
 const video3 = document.getElementById("video3")
 video3.width=200;  //-- Tamaño de la pantalla de video
 video3.height=100;
-var iniciado = false;
+const video4 = document.getElementById("video4")
+video4.width=200;  //-- Tamaño de la imagen
+video4.height=100;
+
+var manual = false;
 var controlador = 1;
+
+var automatic;
+var video_bucle;
+
+var video_sel = 1;
+var bucle = false;
+var inicio_bucle = 0;
 
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
-videoMain.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
-video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
-video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
-video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
+videoMain.poster="https://github.com/nachocru/2019-2020-CSAAI-Practicas/raw/master/P4/Inicio.jpg";
+videoMain.poster.width=400;
+videoMain.poster.height=200;
+video1.src="https://github.com/nachocru/Mis_recursos/raw/master/Video1.mp4"
+video2.src="https://github.com/nachocru/Mis_recursos/raw/master/Video2.mp4"
+video3.src="https://github.com/nachocru/Mis_recursos/raw/master/Video3.mp4"
+video4.src="https://github.com/nachocru/Mis_recursos/raw/master/Video4.mp4"
 
 
 //-- Obtener los botones
 const play1 = document.getElementById("Video1")
 const play2 = document.getElementById("Video2")
 const play3 = document.getElementById("Video3")
+const play4 = document.getElementById("Video4")
 const iniciarNormal = document.getElementById("Inicio1")
 const iniciarAutomatico = document.getElementById("Inicio2")
+const iniciarBucle = document.getElementById("Inicio3")
+const sel_imagen = document.getElementById("Imagen")
 
 //-- Función de retrollamada del botón de ver
 iniciarNormal.onclick = () => {
   console.log("Click iniciar Normal");
+  window.clearInterval(automatic); // Paramos el modo automático si lo hubiera
+  window.clearInterval(video_bucle); // Paramos el modo bucle si lo hubiera
   video1.play();
   video2.play();
   video3.play();
-  iniciado = true;
+  video4.play();
+  bucle = false;
+  manual = true;
 };
 
 function contador() {
@@ -65,39 +86,106 @@ function contador() {
 
 iniciarAutomatico.onclick = () => {
   // En proceso
+  window.clearInterval(video_bucle); // Paramos el modo bucle si lo hubiera
+  manual = false; // Deshabilitamos la opción de cambiar de vídeos
   console.log("Click iniciar Automático");
   video1.play();
   video2.play();
   video3.play();
+  video4.play();
   contador();
-  window.setInterval('contador()',2000);
+  automatic = window.setInterval('contador()',10000);
 };
 
-play1.onclick = () => {
-  if (iniciado == true){
-    console.log("Click vídeo 1");
+iniciarBucle.onclick = () => {
+  console.log("Click iniciar Bucle");
+  window.clearInterval(automatic); // Paramos el modo automático si lo hubiera
+  window.clearInterval(video_bucle);
+  video1.play();
+  video2.play();
+  video3.play();
+  video4.play();
+  bucle = true;
+  manual = true;
+  inicio_bucle = videoMain.currentTime;
+  contador2();
+  video_bucle = window.setInterval('contador2()',10000);
+};
+
+function contador2() {
+  if (video_sel == 1){
     videoMain.src = video1.src;
-    videoMain.currentTime = video1.currentTime;
-    videoMain.play();
+  }else if (video_sel == 2){
+    videoMain.src = video2.src;
+  }else if (video_sel == 3){
+    videoMain.src = video3.src;
+  }
+  videoMain.currentTime = inicio_bucle;
+  videoMain.play();
+}
+
+play1.onclick = () => {
+  if (manual == true){
+    video_sel = 1;
+    console.log("Click vídeo 1");
+    if (bucle == true){
+      window.clearInterval(video_bucle);
+      inicio_bucle = video1.currentTime;
+      video_bucle = window.setInterval('contador2()',10000);
+    } else {
+      videoMain.src = video1.src;
+      videoMain.currentTime = video1.currentTime;
+      videoMain.play();
+    }
   }
 };
 
+
 play2.onclick = () => {
-  if (iniciado == true){
+  if (manual == true){
+    video_sel = 2;
     console.log("Click vídeo 2");
-    videoMain.src = video2.src;
-    videoMain.currentTime = video2.currentTime;
-    videoMain.play();
+    if (bucle == true){
+      window.clearInterval(video_bucle);
+      inicio_bucle = video2.currentTime;
+      video_bucle = window.setInterval('contador2()',10000);
+    } else {
+      videoMain.src = video2.src;
+      videoMain.currentTime = video2.currentTime;
+      videoMain.play();
+    }
   }
 };
 play3.onclick = () => {
-  if (iniciado == true){
+  if (manual == true){
+    video_sel = 3;
     console.log("Click vídeo 3");
-    videoMain.src = video3.src;
-    videoMain.currentTime = video3.currentTime;
-    videoMain.play();
+    if (bucle == true){
+      window.clearInterval(video_bucle);
+      inicio_bucle = video3.currentTime;
+      video_bucle = window.setInterval('contador2()',10000);
+    } else {
+      videoMain.src = video3.src;
+      videoMain.currentTime = video3.currentTime;
+      videoMain.play();
+    }
   }
 };
+
+play4.onclick = () => {
+  console.log("Emision en pruebas");
+  window.clearInterval(automatic); // Paramos el modo automático si lo hubiera
+  window.clearInterval(video_bucle); // Paramos el modo bucle si lo hubiera
+  video1.play();
+  video2.play();
+  video3.play();
+  video4.play();
+  videoMain.src = video4.src;
+  videoMain.currentTime = video4.currentTime;
+  videoMain.play();
+  manual = true;
+  bucle = false;
+}
 //-- Funcion de retrollamada del boton de parar
 //stop.onclick = () => {
   //videoMain.pause();
